741ecf68db7d107548afd61a0baf8f65dd473688
Eugeniu Burduja <eugeniu.burduja@amdaris.com>
2023-08-01 18:27:04 +0300

    Finished saving handler

--- src/app/controllers/user/handler.go
+++ src/app/controllers/user/handler.go
@@ -1,7 +1,9 @@
 package userHandler
 
 import (
+	userModel "bank-app/src/app/controllers/user/models"
 	"bank-app/src/app/controllers/user/services"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -20,7 +22,40 @@ func (userHandler *UserHandler) CreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	fmt.Print(r.Body)
+	var data struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	// TODO: validation...
+
+	user := &userModel.User{
+		Name:     data.Name,
+		Email:    data.Email,
+		Password: data.Password,
+	}
+	fmt.Println(user)
+	err = userHandler.s.CreateUser(user)
+	if err != nil {
+		http.Error(w, "Cannot create user", http.StatusBadRequest)
+		return
+	}
+
+	result, err := json.Marshal(user)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	i, err := w.Write(result)
+	if err != nil {
+		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func (userHandler *UserHandler) GetUserBalance(w http.ResponseWriter, r *http.Request) {
