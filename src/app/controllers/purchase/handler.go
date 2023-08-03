165b1394d052a5fcff936007ddf31ae20a140b94
Eugeniu Burduja <eugeniu.burduja@amdaris.com>
2023-08-03 13:36:00 +0300

    Finished purhcas handlers

--- src/app/controllers/purchase/handler.go
+++ src/app/controllers/purchase/handler.go
@@ -1,7 +1,10 @@
 package purchase
 
 import (
+	purchaseModel "bank-app/src/app/controllers/purchase/models"
 	purchaseService "bank-app/src/app/controllers/purchase/services"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -9,10 +12,63 @@ type PurchaseHandler struct {
 	s *purchaseService.PurchaseServiceI
 }
 
-func (p *PurchaseHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+func (p *PurchaseHandler) CreateGood(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
+	var data struct {
+		Title       string  `json:"title"`
+		Price       float64 `json:"price"`
+		Description string  `json:"description"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("Bad request", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	good := purchaseModel.Good{
+		Title:       data.Title,
+		Price:       data.Price,
+		Description: data.Description,
+	}
+
+	err = p.s.CreateGood(&good)
+	if err != nil {
+		fmt.Println("Cannot create good", err)
+		http.Error(w, "Cannot create good", http.StatusBadRequest)
+		return
+	}
+
+	result, _ := json.Marshal(good)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(result)
 }
 
 func (p *PurchaseHandler) BuyGood(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
+	var data struct {
+		UserID int `json:"user_id"`
+		GoodID int `json:"good_id"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		fmt.Println("Bad request", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	err = p.s.BuyGood(data.UserID, data.GoodID)
+	if err != nil {
+		fmt.Println("Cannot buy good", err)
+		http.Error(w, "Cannot buy good", http.StatusBadRequest)
+		return
+	}
 }
