dc3e2e8968c89f90782bf444e4c4254222ac84a0
Eugeniu Burduja <eugeniu.burduja@amdaris.com>
2023-09-03 12:43:52 +0300

    Added a test

---
+++ src/app/controllers/user/models/user_test.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"bank-app/pkg/database/sqlite3"
+	"fmt"
+	"testing"
+)
+
+func TestMain(t *testing.M) {
+	fmt.Println("hello world")
+	a := t.Run()
+	fmt.Println("END", a)
+}
+
+func TestGetById(t *testing.T) {
+	storage, err := sqlite3.New(":memory:")
+	defer storage.DB.Close()
+
+	if err != nil {
+		t.Fatal("Cannot create db for tests")
+	}
+
+	db := storage.DB
+
+	_, err = db.Exec(`
+		INSERT INTO users (name, email, password, amount) 
+		VALUES (?, ?, ?, ?)`,
+		"test1", "test1@m", "123", 100.00,
+	)
+	if err != nil {
+		t.Fatalf("Failed executing insert %s", err)
+	}
+
+	repo := NewUserRepository(db)
+
+	user, err := repo.GetById(1)
+
+	expectedUser := &User{
+		ID:       1,
+		Name:     "test1",
+		Email:    "test1@m",
+		Password: "123",
+		Amount:   100.00,
+	}
+
+	if user.Name != expectedUser.Name {
+		t.Errorf("Expected name to be %s, but got %s", expectedUser.Name, user.Name)
+	}
+	if user.Email != expectedUser.Email {
+		t.Errorf("Expected email to be %s, but got %s", expectedUser.Email, user.Email)
+	}
+	if user.Password != expectedUser.Password {
+		t.Errorf("Expected password to be %s, but got %s", expectedUser.Password, user.Password)
+	}
+	if user.Amount != expectedUser.Amount {
+		t.Errorf("Expected amoun to be %f, but got %f", expectedUser.Amount, user.Amount)
+	}
+
+}
